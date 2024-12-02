e51618a4edecc999c52e68264a648e1bcfbc2af2
Tyler-200 <hughestyler2000@gmail.com>
2024-12-02 18:00:52 +0000

    Interface and styling added modified:     index.html     style.css

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 /** naughts and crosses JavaScript
  *  Author: Tyler Hughes
  */
+
+const statusMesage = document.querySelector('.status');
+
+let active = true;
+let currentPlayer = "X";
+let gameState = ["","","","","","","","",""];
+const winningConditions = [ [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], 
+                            [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+
+
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
+document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
